21212b762fabf6a3ef0c135398459ef375710c35
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-10-31 12:04:41 -0400

    simple fixes

--- grid_rotate.py
+++ grid_rotate.py
@@ -5,13 +5,13 @@ FRAMES = 60
 def drawLine(angle, scl):
     fill(None)
     stroke(1)
-    strokeWidth(1/scl)
+    strokeWidth(1 / scl)
     save()
     scale(scl)
-    translate(PAGESIZE/5, PAGESIZE/5)
-    rotate(angle*5)
-    translate(-PAGESIZE/5, -PAGESIZE/5)
-    line((0, 0), (PAGESIZE/2.5, PAGESIZE/2.5))
+    translate(PAGESIZE / 5, PAGESIZE / 5)
+    rotate(angle * 5)
+    translate(-PAGESIZE / 5, -PAGESIZE / 5)
+    line((0, 0), (PAGESIZE / 2.5, PAGESIZE / 2.5))
     restore()
 
 for frame in range(FRAMES):
@@ -21,12 +21,12 @@ for frame in range(FRAMES):
     rect(0, 0, PAGESIZE, PAGESIZE)
     translate(-PAGESIZE / TOTALROWS / 1.5, 0)
 
-    for col in range(TOTALCOLS*2):
+    for row in range(TOTALROWS * 2):
         save()
-        translate(0, col*PAGESIZE/TOTALCOLS/2)
-        for row in range(TOTALROWS*3):
-            drawLine(f * row, 1 / TOTALROWS)
-            translate(PAGESIZE / TOTALROWS / 2, 0)
+        translate(0, row * PAGESIZE / TOTALCOLS/2)
+        for col in range(TOTALCOLS * 3):
+            drawLine(f * col, 1 / TOTALCOLS)
+            translate(PAGESIZE / TOTALCOLS / 2, 0)
         restore()
 
 saveImage("_gifs/grid_rotate.gif")
